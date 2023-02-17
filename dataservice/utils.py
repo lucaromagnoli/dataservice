16a3ad23df9be37bee1d9074cdefe53d2fb5ba30
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-17 12:01:22 +0000

    black

--- dataservice/utils.py
+++ dataservice/utils.py
@@ -4,5 +4,8 @@ from typing import Callable, Any, Coroutine, TypeVar
 
 Nothing = TypeVar("Nothing")
 
-def async_to_sync(coro: Callable[[Any], Coroutine[Any, Any, Nothing]], *args, **kwargs) -> Any:
+
+def async_to_sync(
+    coro: Callable[[Any], Coroutine[Any, Any, Nothing]], *args, **kwargs
+) -> Any:
     return asyncio.run(coro(*args, **kwargs))
