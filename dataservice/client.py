6440ac55d098697a637707b97578604e5f16b53f
lucaromagnoli <romagnoli.luca@gmail.com>
2023-02-18 11:26:32 +0000

    remove get and post

--- dataservice/client.py
+++ dataservice/client.py
@@ -2,20 +2,22 @@ import asyncio
 import random
 from logging import getLogger
 
-from dataservice.http import Request, Response
-from dataservice.logger import DataServiceLoggerAdapter
-
-logger = getLogger(__name__)
+from dataservice.messages import Request, Response
 
 
 class Client:
     def __init__(self):
-        self.logger = DataServiceLoggerAdapter(
-            logger, {"module": self.__class__.__name__}
-        )
+        self.logger = getLogger(__name__)
+
+    async def make_request(self, request: Request) -> Response:
+        raise NotImplementedError
+
+
+class ToyClient(Client):
 
     async def make_request(self, request: Request) -> Response:
         self.logger.info(f"Requesting {request.url}")
         await asyncio.sleep(random.randint(0, 200) / 100)
         self.logger.info(f"Returning response for {request.url}")
-        return Response(request=request, data="response data")
+        data = f"<html><head></head><body>This is content for URL: {request.url}</body></html>"
+        return Response(request=request, data=data)
