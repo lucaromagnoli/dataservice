f49cfbe2a74871b22b3a49318960461baef6e95e
lucaromagnoli <romagnoli.luca@gmail.com>
2024-07-13 12:38:08 +0100

    refactor messages.py

--- dataservice/messages.py
+++ dataservice/messages.py
@@ -8,7 +8,7 @@ DataItemGeneric = TypeVar("DataItemGeneric")
 RequestOrData = Union["Request", DataItemGeneric]
 CallbackReturn = Iterator[RequestOrData] | RequestOrData
 CallbackType = Callable[["Response"], CallbackReturn]
-ResponseData = str | dict
+StrOrDict = str | dict
 
 
 @dataclass
@@ -16,16 +16,23 @@ class Request:
     url: AnyUrl
     callback: CallbackType
     method: Literal["GET", "POST"] = "GET"
-    content_type: Literal["text", "data"] = "text"
+    content_type: Literal["text", "json"] = "text"
     client: Optional[str] = None
 
 
-@dataclass
 class Response:
-    request: Request
-    data: ResponseData
-    parser: str = "html5lib"
+    def __init__(self, request: Request, data: StrOrDict):
+        self.request = request
+        self.data = data
+        self.__soup = None
+
+    def __get_soup(self):
+        if isinstance(self.data, dict):
+            raise Warning("Cannot create BeautifulSoup from dict.")
+        return BeautifulSoup(self.data, "html5lib")
 
     @property
-    def soup(self):
-        return BeautifulSoup(self.data, "html5lib")
+    def soup(self) -> BeautifulSoup:
+        if self.__soup is None:
+            self.__soup = self.__get_soup()
+        return self.__soup
