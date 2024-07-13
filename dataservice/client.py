12ed4f8379c73907f08bffd5bcc27536e39366d5
lucaromagnoli <romagnoli.luca@gmail.com>
2024-07-13 12:21:36 +0100

    add HTTPX Client and remove ToyClient

--- dataservice/client.py
+++ dataservice/client.py
@@ -1,25 +1,41 @@
 import asyncio
 import random
+from abc import ABC, abstractmethod
 from logging import getLogger
+
+import httpx
 
 from dataservice.messages import Request, Response
 
 
-class Client:
+class Client(ABC):
     def __init__(self):
         self.logger = getLogger(__name__)
 
     def get_name(self):
         return self.__class__.__name__
 
+    @abstractmethod
     async def make_request(self, request: Request) -> Response:
         raise NotImplementedError
 
 
-class ToyClient(Client):
+class HTTPXClient(Client):
+    def __init__(self):
+        super().__init__()
+        self.async_client = httpx.AsyncClient
+
     async def make_request(self, request: Request) -> Response:
         self.logger.info(f"Requesting {request.url}")
-        await asyncio.sleep(random.randint(0, 200) / 100)
+        async with self.async_client() as client:
+            response = await client.get(request.url)
+            response.raise_for_status()
+            match request.content_type:
+                case "text":
+                    data = response.text
+                case "json":
+                    data = response.json()
         self.logger.info(f"Returning response for {request.url}")
-        data = f"<html><head></head><body>This is content for URL: {request.url}</body></html>"
         return Response(request=request, data=data)
+
+
