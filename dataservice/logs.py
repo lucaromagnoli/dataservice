28e5e01cd9cb974b0dca4c1fffcfdb066e58fce2
lucaromagnoli <romagnoli.luca@gmail.com>
2024-08-12 22:13:34 +0200

    improve logs and tests

--- dataservice/logs.py
+++ dataservice/logs.py
@@ -1,35 +1,41 @@
 """Logging module."""
 
-import logging.config
+import logging
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+HandlerType = Literal["stdout", "file"]
+LoggingLevel = Literal["INFO", "DEBUG", "WARNING", "ERROR"]
 
 
-def setup_logging():
-    config_dict = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "simple": {
-                "format": "%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s",
-            }
-        },
-        "handlers": {
-            "stdout": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://sys.stdout",
-                "formatter": "simple",
-            }
-        },
-        "loggers": {
-            "dataservice": {
-                "handlers": ["stdout"],
-                "level": "DEBUG",
-                "propagate": False,
-            },
-            "books_scraper": {
-                "handlers": ["stdout"],
-                "level": "INFO",
-                "propagate": False,
-            },
-        },
+class Handler(BaseModel):
+    class_: str = Field(alias="class", default="logging.StreamHandler")
+    stream: str = "ext://sys.stdout"
+    formatter: str = "simple"
+
+
+class Handlers(BaseModel):
+    stdout: Handler = Handler()
+
+
+class LoggerDict(BaseModel):
+    handlers: list[HandlerType] = ["stdout"]
+    level: LoggingLevel = "DEBUG"
+    propagate: bool = False
+
+
+class LoggingConfigDict(BaseModel):
+    version: int = 1
+    disable_existing_loggers: bool = False
+    formatters: dict[str, dict] = {
+        "simple": {"format": "%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s"},
     }
+    handlers: Handlers = Handlers()
+    loggers: dict[str, LoggerDict] = {"dataservice": LoggerDict()}
+
+
+def setup_logging(config_dict: LoggingConfigDict | None = None):
+    if config_dict is None:
+        config_dict = LoggingConfigDict().model_dump(by_alias=True)
     logging.config.dictConfig(config_dict)
---
+++ tests/unit/test_logs.py
@@ -0,0 +1,43 @@
+import pytest
+
+from dataservice.logs import LoggingConfigDict, setup_logging
+
+
+@pytest.fixture
+def init_config_dict():
+    return {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s",
+            }
+        },
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "simple",
+            }
+        },
+        "loggers": {
+            "dataservice": {
+                "handlers": ["stdout"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+        },
+    }
+
+
+def test_init_config_dict(init_config_dict):
+    config_dict = LoggingConfigDict().model_dump(by_alias=True)
+    assert config_dict == init_config_dict
+
+
+def test_setup_logging_default(mocker):
+    mocked_dict_config = mocker.patch("dataservice.logs.logging.config.dictConfig")
+    setup_logging()
+    mocked_dict_config.assert_called_once_with(
+        LoggingConfigDict().model_dump(by_alias=True)
+    )
