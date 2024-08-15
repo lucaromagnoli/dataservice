6710e9c12534a9d72b6cc0c4fed43631b5ea75f6
lucaromagnoli <romagnoli.luca@gmail.com>
2024-08-15 23:06:28 +0200

    use furo theme

--- source/conf.py
+++ source/conf.py
@@ -20,7 +20,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = []
 
-html_theme = "pydata_sphinx_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 autodoc_pydantic_model_show_json = False
