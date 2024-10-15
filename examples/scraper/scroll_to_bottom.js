62200d5f2de3d74dcc1347f7e3b9945944cc374e
lucaromagnoli <romagnoli.luca@gmail.com>
2024-10-15 09:01:58 +0100

    put consts at the top

--- examples/scraper/scroll_to_bottom.js
+++ examples/scraper/scroll_to_bottom.js
@@ -1,11 +1,11 @@
 (async () => {
   const scroll = async () => {
-    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-    const scrollHeight = () => document.body.scrollHeight;
-    const start = 0;
-    const shouldStop = (position) => position > scrollHeight();
     const increment = 100;
     const delayTime = 100;
+    const start = 0;
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const scrollHeight = () => document.body.scrollHeight;
+    const shouldStop = (position) => position > scrollHeight();
     console.error(start, shouldStop(start), increment);
     for (let i = start; !shouldStop(i); i += increment) {
       window.scrollTo(0, i);
