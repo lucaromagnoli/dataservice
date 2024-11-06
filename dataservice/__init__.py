70984da532d1099225dfdb2a1b2d0476dca3b26a
Nomad Monad <38782977+lucaromagnoli@users.noreply.github.com>
2024-11-06 18:32:58 +0000

    remove check playwright in init (#63)

--- dataservice/__init__.py
+++ dataservice/__init__.py
@@ -1,5 +1,4 @@
 from dataservice.clients import (
-    PLAYWRIGHT_AVAILABLE,
     HttpXClient,
     PlaywrightClient,
     PlaywrightPage,
@@ -16,19 +15,6 @@ from dataservice.exceptions import DataServiceException, RetryableException
 from dataservice.logs import setup_logging
 from dataservice.models import FailedRequest, Request, Response
 from dataservice.service import AsyncDataService, DataService
-
-
-def check_playwright_availability():
-    if not PLAYWRIGHT_AVAILABLE:
-        raise ImportError(
-            "Playwright optional dependency is not installed. Please install it with `pip install python-dataservice[playwright]`."
-        )
-
-
-# Check availability when importing PlaywrightClient or PlaywrightPage
-if "PlaywrightClient" in globals() or "PlaywrightPage" in globals():
-    check_playwright_availability()
-
 
 __all__ = [
     "AsyncDataService",
