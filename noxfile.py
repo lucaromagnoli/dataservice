370072014f19609dded7ed4004dc3d388c49d61d
lucaromagnoli <romagnoli.luca@gmail.com>
2024-11-14 20:02:41 +0000

    add "--ignore=tests/integration/test_pw_extra_http_headers.py" to nox

--- noxfile.py
+++ noxfile.py
@@ -8,4 +8,11 @@ nox.options.error_on_missing_interpreters = True
 @nox_poetry.session
 def tests(session):
     session.install(".")
-    session.run("pytest", "tests/", "-v", "--cov=dataservice/", external=True)
+    session.run(
+        "pytest",
+        "tests/",
+        "-v",
+        "--cov=dataservice/",
+        "--ignore=tests/integration/test_pw_extra_http_headers.py",
+        external=True,
+    )
